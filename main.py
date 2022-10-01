6db20823a21ba906d22389330b4b2258e4eda522
Igor Molnar <iggystrat@gmail.com>
2022-10-01 16:36:21 +0200

    expand evaluation metrics, tune optimizer,

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import gc
 import logging
 import os
+import pickle
 import time
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 import torch.nn as nn
+import sklearn.metrics as metrics
 from torch import optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
@@ -75,8 +79,11 @@ def analyze_results(model=None, saved_mfile=None, channels=None):
 
 
 class FashionClassifier:
-    def __init__(self, mini_batch_sz):
+    def __init__(self, epochs=500, lr=0.1, mini_batch_sz=256, version=1):
+        self.epochs = epochs
+        self.lr = lr
         self.mini_batch_sz = mini_batch_sz
+        self.ver = version
         self._train_loader = None
         self._test_loader = None
 
@@ -126,34 +133,39 @@ class FashionClassifier:
 
     def evaluate_model(self, model):
         """
-        Evaluate the Top-1 error rate on both train and test datasets
+        Evaluate various performance metrics on both train and test datasets
         """
         # TODO: Calculate and display other metrics, precision, recall, accuracy
         #   plot the confusion matrix at the end of the pipeline
         results = {}
         for name, loader in [("train", self.train_loader),
                              ("test", self.test_loader)]:
-            mistakes = 0
-            total = 0
 
+            lbls = []
+            pred = []
             with torch.no_grad():
                 for imgs, labels in loader:
-                    imgs = imgs.to(device=device)
-                    labels = labels.to(device=device)
                     outputs = model(imgs)
                     _, predicted = torch.max(outputs, dim=1)
-                    total += labels.shape[0]
-                    mistakes += int((predicted != labels).sum())
+                    lbls.append(labels)
+                    pred.append(predicted)
 
-            err = mistakes / total
-            results[name] = err
+            labels = torch.cat(lbls).cpu()
+            pred = torch.cat(pred).cpu()
+
+            # micro = global stats
+            # macro = per label stats then mean
+            acc = metrics.accuracy_score(labels, pred)
+            prec = metrics.precision_score(labels, pred, average='micro')
+            m = metrics.confusion_matrix(labels, pred)
+            err = 1 - acc
+            results[name] = (m, err, prec)
         return results
 
     def train(self, n_epochs, optimizer, lr_scheduler, model, loss_fn):
         start = time.time()
         res = dict()
         for n in range(1, n_epochs + 1):
-            loss_train = 0.0
             for imgs, labels in self.train_loader:
                 outputs = model(imgs)
                 loss = loss_fn(outputs, labels)
@@ -162,23 +174,32 @@ class FashionClassifier:
                 loss.backward()
                 optimizer.step()
 
-                loss_train += loss.item()
-
+            # evaluation and logging
             errors = self.evaluate_model(model)
             res[n] = errors
-            logger.info("Ep {}, time {:d}s, err(%) - {}: {:.1f}, {}: {:.1f}".format(
-                n, round(time.time() - start),
-                'train', 100 * errors['train'], 'test', 100 * errors['test']))
 
-            lr_scheduler.step(errors['test'])
+            if n % 10 == 0:
+                m, err, _ = errors['test']
+                plt.imshow(m, cmap='gray')
+                plt.show()
+            logger.info(
+                "Ep {}, time {:d}s, err(%) - {}: {:.1f}, {}: {:.1f}".format(
+                    n, round(time.time() - start),
+                    'train', 100 * errors['train'][1],
+                    'test', 100 * errors['test'][1]))
 
-    def main(self, model_name, epochs, lr):
+            if lr_scheduler is not None:
+                lr_scheduler.step(errors['test'][1])
+        return model, res
+
+    def main(self):
         model = MyResNet()
 
-        opt = optim.SGD(model.parameters(), lr=lr)
+        opt = optim.SGD(model.parameters(), lr=self.lr,
+                        weight_decay=1e-4, momentum=0.9)
         lr_sched = ReduceLROnPlateau(opt, patience=25)
-        self.train(
-            n_epochs=epochs,
+        model, res = self.train(
+            n_epochs=self.epochs,
             optimizer=opt,
             lr_scheduler=lr_sched,
             model=model,
@@ -207,13 +228,10 @@ def calculate_patience(df=None):
 #   - augmentation (random crops/horizontal, vertical flips)
 #   - random translation
 #   - random rotation
-# TODO: Compute train vs test accuracy depending on number of epochs
 def main():
-    epochs = 1000
-    lr = 1e-1
-    mini_batch_sz = 256
-    model = FashionClassifier(mini_batch_sz).main('myresnet1', epochs, lr)
-    return
+    fc = FashionClassifier(epochs=1000, lr=1e-1,
+                           mini_batch_sz=1024, version=3)
+    fc.main()
 
 
 if __name__ == '__main__':
